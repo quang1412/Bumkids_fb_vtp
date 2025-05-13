e82b3ed74f9babf2663e97cbff8ff339c39785de
QuangPlus <trinhdacquang@gmail.com>
2025-05-13 18:13:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bum | FB - VTP
 // @author       QuangPlus
-// @version      2025-05-13
+// @version      2025-05-13-1
 // @description  try to take over the world!
 // @namespace    Bumkids_fb_vtp
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=viettelpost.vn
